2b60b394c1d124b991ed103903f85dd65d4ac189
MansourMoukdad <moukdad1@gmail.com>
2024-11-17 09:17:40 +0300

    Starting with c++

---
+++ Fundamentals-of-c+/app.cpp
@@ -0,0 +1,14 @@
+// Your First C++ Program
+
+#include <iostream>
+
+int main() {
+    int price = 200;
+    std::cout << "Price is " << price;
+    std::cout << "\nPrice is after adding 15 is " << price + 15;
+    std::cout << "\nPrice is after adding 50 is " << price + 50;
+    price = 150;
+    std::cout << "\nThe new Price is " << price;
+
+    return 100;
+}
