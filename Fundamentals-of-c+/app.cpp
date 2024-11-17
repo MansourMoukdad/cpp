1a3cfb0158badd1a3639cb4f741f61435a49bdf2
MansourMoukdad <moukdad1@gmail.com>
2024-11-17 09:22:37 +0300

    using namespace std

--- Fundamentals-of-c+/app.cpp
+++ Fundamentals-of-c+/app.cpp
@@ -1,14 +1,14 @@
 // Your First C++ Program
 
 #include <iostream>
-
+    using namespace std;
 int main() {
     int price = 200;
-    std::cout << "Price is " << price;
-    std::cout << "\nPrice is after adding 15 is " << price + 15;
-    std::cout << "\nPrice is after adding 50 is " << price + 50;
+    cout << "Price is " << price;
+    cout << "\nPrice is after adding 15 is " << price + 15;
+    cout << "\nPrice is after adding 50 is " << price + 50;
     price = 150;
-    std::cout << "\nThe new Price is " << price;
+    cout << "\nThe new Price is " << price;
 
     return 100;
 }
