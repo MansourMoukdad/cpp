bbccb8180a4b8fcb914dcbc3152f8c52400111b5
MansourMoukdad <moukdad1@gmail.com>
2024-11-29 14:36:01 +0300

    [Create] Operators – Arithmetic Operators

---
+++ Fundamentals-of-c+/Operators_ArithmeticOperators.cpp
@@ -0,0 +1,16 @@
+/*
+  Operators & Operands
+  "Symbols To Operate On Data"
+
+  - Arithmetic Operators
+  "For Mathematical Operations"
+
+  --- + => Addition
+  --- - => Subtraction
+  --- * => Multiplication
+  --- / => Division
+  --- % => Modulo => Remainder After Division
+
+  What Is Operand ?
+  - The Part Of an Instruction Representing The Data Manipulated by The Operator
+*/
