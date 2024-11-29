54b401693b35d597043c51540b06c0099e228323
MansourMoukdad <moukdad1@gmail.com>
2024-11-29 14:07:29 +0300

    Create Type Conversion

---
+++ Fundamentals-of-c+/DataTypes_TypeConversion.cpp
@@ -0,0 +1,15 @@
+/*
+  Data Types
+  - Type Conversion
+  --- Convert Data of One Type To Another
+
+  - Implicit Conversion
+  --- Conversion Is Done Automatically By The Compiler
+
+  - Explicit Conversion AKA Type Casting
+  --- Conversion Is Done By The Programmer
+
+  - Data Loss
+  --- When Larger Type Is Converted To Smaller Type
+*/
+
