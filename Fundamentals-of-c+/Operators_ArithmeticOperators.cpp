7b1e5830eb460f166336762503995f15fb2d734b
MansourMoukdad <moukdad1@gmail.com>
2024-11-30 09:36:27 +0300

    Arithmetic Operators

--- Fundamentals-of-c+/Operators_ArithmeticOperators.cpp
+++ Fundamentals-of-c+/Operators_ArithmeticOperators.cpp
@@ -14,3 +14,34 @@
   What Is Operand ?
   - The Part Of an Instruction Representing The Data Manipulated by The Operator
 */
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  cout << 10 + 10 << "\n";
+  cout << sizeof(10 + 10) << "\n";
+  cout << 10.5 + 9.5 << "\n";
+  cout << sizeof(10.5f + 9.5f) << "\n";
+  cout << int(10.5f + 9.5f) << "\n";
+
+  cout << "=====================\n";
+  cout << sizeof(10.5 + 9.5) << "\n"; // 8 => Double
+  cout << sizeof(int(10.5 + 9.5)) << "\n"; // 4 => integer
+
+  cout << 100 - 50 << "\n";  
+  cout << 100 - -50 << "\n";  
+
+  cout << 10 * 5 << "\n";
+  cout << 20 / 5 << "\n"; 
+
+  cout << 12 / 5 << "\n";
+  cout << 12.f / 5.f << "\n";
+
+  cout << 20 % 5 << "\n";  // 0
+  cout << 21 % 5 << "\n";  // 1
+  cout << 24 % 5 << "\n";  // 4
+  // cout << 24.5 % 5 << "\n";  // Problem
+  return 0;
+}
