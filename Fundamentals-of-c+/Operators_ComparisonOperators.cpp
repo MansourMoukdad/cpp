a0c702f8b5a3f6ee1e9e6166c4f64d7e2bacd1c8
MansourMoukdad <moukdad1@gmail.com>
2024-11-30 10:29:12 +0300

    Comparison Operators

--- Fundamentals-of-c+/Operators_ComparisonOperators.cpp
+++ Fundamentals-of-c+/Operators_ComparisonOperators.cpp
@@ -12,3 +12,26 @@
   --- >= Greater Than Or Equal
   --- <= Less Than Or Equal
 */
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  cout << (10 == 10) << "\n"; // 1 => True 
+  cout << (1000 == 100) << "\n"; // 0 => False 
+
+  cout << (10 != 10) << "\n"; // 0 => False 
+  cout << (1000 != 100) << "\n"; // 1 => True
+
+  cout << (40 > 18) << "\n"; // 1 => True 
+  cout << (18 > 18) << "\n"; // 0 => False 
+
+  cout << (16 < 18) << " \n"; // 1 => True 
+ 
+  cout << (40 >= 18) << "\n"; // 1 => True 
+  cout << (18 >= 18) << "\n"; // 1 => True
+
+
+  return 0;
+}
