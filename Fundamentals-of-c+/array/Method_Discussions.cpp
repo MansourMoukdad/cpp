3177eb2e795728f036b1e36bd5a7adced675dc42
MansourMoukdad <moukdad1@gmail.com>
2024-12-02 18:04:01 +0300

    Methods Discussions

---
+++ Fundamentals-of-c+/array/Method_Discussions.cpp
@@ -0,0 +1,28 @@
+/*
+  Arrays
+  - Useful Methods
+  --- at
+  --- front
+  --- back
+  --- fill
+  --- size
+  --- empty
+*/
+
+#include <array>
+#include <iostream>
+using namespace std;
+
+int main()
+{   
+    array<int, 4> nums = {100, 200, 300, 400};
+    cout << nums[0] << "\n";    // 100
+    cout << nums.front() << "\n"; // 100
+    cout << nums[3] << "\n"; // 400
+    cout << nums.back() << "\n"; // 400
+    cout << nums.at(1) << "\n"; // 200 
+    cout << nums.size() << "\n"; // 4 
+    cout << nums.empty() << "\n"; // 0 false => because it has an elements
+
+    return 0 ;
+}
