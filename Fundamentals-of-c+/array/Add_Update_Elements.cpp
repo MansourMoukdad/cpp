85ac9b23f24013e3820091fe73ed8a0a8545944d
MansourMoukdad <moukdad1@gmail.com>
2024-12-02 17:11:09 +0300

    Add and Update Elements

---
+++ Fundamentals-of-c+/array/Add_Update_Elements.cpp
@@ -0,0 +1,33 @@
+/*
+  Arrays
+  - Declare Empty Array
+  - Add Elements To Array
+  - Update Array Elements
+  - Get Length Of Array With Sizeof
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int nums[4];
+
+    nums[3] = 400;  // Last Element
+    nums[2] = 300;  // Third Element
+    nums[0] = 100;  // First Element
+    nums[1] = 200;  // Second Element
+
+    cout << "Element 1: " <<  nums[0] << "\n";
+    cout << "Element 2: " <<  nums[1] << "\n";
+    cout << "Element 3: " <<  nums[2] << "\n";
+    cout << "Element 4: " <<  nums[3] << "\n";
+
+    nums[1] = 1000;  // Second Element
+    cout << "Element 2: " <<  nums[1] << "\n";
+
+    int anums[] = {100, 200, 300, 400, 500, 600};  // 24 / 4 = 6
+    cout << "Array Elements Count is : " << sizeof(anums) / sizeof(nums[0]) << "\n";
+
+    return 0;
+}
