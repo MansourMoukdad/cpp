bbe220c7722628f606b880959e38d6c7a512a804
MansourMoukdad <moukdad1@gmail.com>
2024-12-04 12:20:41 +0300

    Guess The Number While Loop

--- Fundamentals-of-c+/Loops/Loop_Training.cpp
+++ Fundamentals-of-c+/Loops/Loop_Training.cpp
@@ -29,5 +29,35 @@ int main()
         }
     }
     cout << "Result Is " << result << "\n";
+
+
+
+    // Guess The Number
+
+    int guessNumber = 7;
+    int guessTries = 0;
+    int choose;
+    cout << "Please Guess The Number Between 1 and  10 \n";
+    while (true)
+    {
+        cin >> choose;
+        if(choose == guessNumber) 
+        {
+            cout << "Great, You Guess The Number \n";
+            break;
+        } else 
+        {
+            if(choose != guessNumber)
+            {
+                cout << "Sorry, Wrong Guess, Try Again \n";
+                guessTries++;
+            }
+        }
+        if(guessTries == 3) {
+            cout << "The Number Is 7 \n";
+            break;
+        }
+    }
+    
     return 0;
 }
