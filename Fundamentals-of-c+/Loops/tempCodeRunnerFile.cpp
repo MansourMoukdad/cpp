fa16381d0e54c8f4b87c02773656cd6a7d587b4d
MansourMoukdad <moukdad1@gmail.com>
2024-12-04 12:36:47 +0300

    Add 3 Simple Apps Using Loops

--- Fundamentals-of-c+/Loops/tempCodeRunnerFile.cpp
+++ Fundamentals-of-c+/Loops/tempCodeRunnerFile.cpp
@@ -1,1 +1,42 @@
-<
+    // int result = 0;
+    // int nums[] = {10, 20, -20, 13, 30, -30, 40};
+    // int numSize = sizeof(nums) / sizeof(nums[0]);
+    
+    // for (int i = 0; i < numSize; i++ )
+    // {
+    //     if(nums[i] > 0 && nums[i] % 2 == 0)
+    //     {
+    //         result += nums[i];
+    //         // cout << result + nums[i] << "\n";
+    //     }
+    // }
+    // cout << "Result Is " << result << "\n";
+
+
+
+    // // Guess The Number
+
+    // int guessNumber = 7;
+    // int guessTries = 0;
+    // int choose;
+    // cout << "Please Guess The Number Between 1 and  10 \n";
+    // while (true)
+    // {
+    //     cin >> choose;
+    //     if(choose == guessNumber) 
+    //     {
+    //         cout << "Great, You Guess The Number \n";
+    //         break;
+    //     } else 
+    //     {
+    //         if(choose != guessNumber)
+    //         {
+    //             cout << "Sorry, Wrong Guess, Try Again \n";
+    //             guessTries++;
+    //         }
+    //     }
+    //     if(guessTries == 3) {
+    //         cout << "The Number Is 7 \n";
+    //         break;
+    //     }
+    // }
