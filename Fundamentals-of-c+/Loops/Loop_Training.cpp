bdb09d4965d9fbecf15d9c99719cdc8013a8612d
MansourMoukdad <moukdad1@gmail.com>
2024-12-04 12:35:04 +0300

    Reversed Element From User

--- Fundamentals-of-c+/Loops/Loop_Training.cpp
+++ Fundamentals-of-c+/Loops/Loop_Training.cpp
@@ -16,47 +16,64 @@ using namespace std;
 
 int main()
 {
-    int result = 0;
-    int nums[] = {10, 20, -20, 13, 30, -30, 40};
-    int numSize = sizeof(nums) / sizeof(nums[0]);
+    // int result = 0;
+    // int nums[] = {10, 20, -20, 13, 30, -30, 40};
+    // int numSize = sizeof(nums) / sizeof(nums[0]);
     
-    for (int i = 0; i < numSize; i++ )
+    // for (int i = 0; i < numSize; i++ )
+    // {
+    //     if(nums[i] > 0 && nums[i] % 2 == 0)
+    //     {
+    //         result += nums[i];
+    //         // cout << result + nums[i] << "\n";
+    //     }
+    // }
+    // cout << "Result Is " << result << "\n";
+
+
+
+    // // Guess The Number
+
+    // int guessNumber = 7;
+    // int guessTries = 0;
+    // int choose;
+    // cout << "Please Guess The Number Between 1 and  10 \n";
+    // while (true)
+    // {
+    //     cin >> choose;
+    //     if(choose == guessNumber) 
+    //     {
+    //         cout << "Great, You Guess The Number \n";
+    //         break;
+    //     } else 
+    //     {
+    //         if(choose != guessNumber)
+    //         {
+    //             cout << "Sorry, Wrong Guess, Try Again \n";
+    //             guessTries++;
+    //         }
+    //     }
+    //     if(guessTries == 3) {
+    //         cout << "The Number Is 7 \n";
+    //         break;
+    //     }
+    // }
+
+
+    // Reversed Elements From User
+
+    int vals[5];
+    int inp;
+    cout << "Type 5 Number To Reverse \n";
+    for(int i = 4; i > -1; i--) 
     {
-        if(nums[i] > 0 && nums[i] % 2 == 0)
-        {
-            result += nums[i];
-            // cout << result + nums[i] << "\n";
-        }
+        cin >> inp;
+        vals[i] = inp;
     }
-    cout << "Result Is " << result << "\n";
-
-
-
-    // Guess The Number
-
-    int guessNumber = 7;
-    int guessTries = 0;
-    int choose;
-    cout << "Please Guess The Number Between 1 and  10 \n";
-    while (true)
+    cout << "\n=======================\n";
+    for(int i = 0; i < 5; i++)
     {
-        cin >> choose;
-        if(choose == guessNumber) 
-        {
-            cout << "Great, You Guess The Number \n";
-            break;
-        } else 
-        {
-            if(choose != guessNumber)
-            {
-                cout << "Sorry, Wrong Guess, Try Again \n";
-                guessTries++;
-            }
-        }
-        if(guessTries == 3) {
-            cout << "The Number Is 7 \n";
-            break;
-        }
+        cout << vals[i] << "\n";
     }
     
     return 0;
